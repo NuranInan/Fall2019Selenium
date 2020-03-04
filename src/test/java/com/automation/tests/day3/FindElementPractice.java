ec87268fc98eb96a072b216074910f7b7de1de2f
NuranInan <nurdingil@gmail.com>
2020-03-03 20:16:51 -0500

    day 3

--- src/test/java/com/automation/tests/day3/FindElementPractice.java
+++ src/test/java/com/automation/tests/day3/FindElementPractice.java
@@ -2,7 +2,9 @@ package com.automation.tests.day3;
 
 import com.automation.utilities.DriverFactory;
 import io.github.bonigarcia.wdm.WebDriverManager;
+import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
 import org.openqa.selenium.chrome.ChromeDriver;
 
 public class FindElementPractice {
@@ -12,10 +14,18 @@ public class FindElementPractice {
         //WebDriver driver = new ChromeDriver();
         WebDriver driver = DriverFactory.createADriver("chrome");
         driver.get("http://practice.cybertekschool.com/sign_up");
+        WebElement fullName = driver.findElement(By.name("full_name"));
+        fullName.sendKeys("Mister Twister");
+        WebElement email = driver.findElement(By.name("email"));
+        email.sendKeys("sdet@cybertek.com");
+
+        WebElement signUp = driver.findElement(By.name("wooden_spoon"));
+        //When you see type="submit"you can use submit instead of click()
+        //it make sense to use when click() method doesn't work
+        signUp.submit();
 
 
-
-        driver.quit();//to close everthing
+        driver.quit();//to close everything
 
     }
 }
