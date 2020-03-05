0672828fe240dad134825601281f3c26b7a2930c
NuranInan <nurdingil@gmail.com>
2020-03-04 20:28:11 -0500

    day 4

--- src/test/java/com/automation/utilities/DriverFactory.java
+++ src/test/java/com/automation/utilities/DriverFactory.java
@@ -1,13 +1,21 @@
 package com.automation.utilities;
 
 import io.github.bonigarcia.wdm.WebDriverManager;
+import org.openqa.selenium.PageLoadStrategy;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.chrome.ChromeOptions;
 import org.openqa.selenium.firefox.FirefoxDriver;
 
 public class DriverFactory {
-
-    public static WebDriver createADriver(String browserName){
+    /**
+     * You homework will be to improve this method!
+     * This method return webdriver object based on browser type
+     * If you want to use chrome browser, just provide chrome as a parameter
+     * @param browserName
+     * @return webdriver object
+     */
+    public static WebDriver createDriver(String browserName){
         if(browserName.equalsIgnoreCase("chrome")){
             WebDriverManager.chromedriver().setup();
             return new ChromeDriver();
